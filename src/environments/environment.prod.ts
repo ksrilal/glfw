40d950b2b19b9375e118cef9dd54b838355ebfc3
MaheemaPavithrani <maheemapavithrani@gmail.com>
2019-11-04 17:21:40 +0530

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-<<<<<<< HEAD
     apiKey: "AIzaSyDnQnxvQrAW17USKB1wmbgE0TEZXj-X7mE",
     authDomain: "glf123.firebaseapp.com",
     databaseURL: "https://glf123.firebaseio.com",
@@ -10,15 +9,5 @@ export const environment = {
     messagingSenderId: "139680442955",
     appId: "1:139680442955:web:8b469836b4945d2cb77bce",
     measurementId: "G-010QF6MB20"
-=======
-    apiKey: 'AIzaSyDnQnxvQrAW17USKB1wmbgE0TEZXj-X7mE',
-    authDomain: 'glf123.firebase.app.com',
-    databaseURL: 'https://glf123.firebaseio.com',
-    projectId: 'glf123',
-    storageBucket: 'glf123.appspot.com',
-    messagingSenderId: '139680442955',
-    appId: '1:139680442955:web:8b469836b4945d2cb77bce',
-    measurementId: 'G-010QF6MB20'
->>>>>>> b7e6660f80d975bfdf6b2ff9c64a988f84d180e8
   }
 };
